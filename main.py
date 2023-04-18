9699b6914c9e6ff128c0ad8a11c6e34f96a357cb
Carlos Magno <120645803+CM4gno@users.noreply.github.com>
2023-04-18 14:15:58 -0300

    Atualizar 'pxt.json, main.blocks, main.ts, main.py'

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 def on_button_pressed_a():
-    sprite.move(1)
+    if sprite.get(LedSpriteProperty.X) == 2:
+        game.add_score(1)
+    else:
+        game.remove_life(1)
+        basic.show_leds("""
+            . . . . .
+                        . # . # .
+                        . . . . .
+                        . # # # .
+                        # . . . #
+        """)
+        music.play_melody("C5 G B A F A C5 B ", 500)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    sprite.turn(Direction.RIGHT, 45)
+    basic.show_string("FASE SECRETA")
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 sprite: game.LedSprite = None
-music.play_melody("E B C5 A B G A F ", 120)
-basic.show_string("Oi, seja Bem Vindo!")
-basic.show_string("Esse é somente um jogo teste")
-basic.show_string("Espero que goste!")
+music.play_melody("C4:4 D4:4 E4:4 C4:4 D4:4 E4:4 C4:2", 120)
+basic.show_string("OI")
+game.add_life(5)
+sprite = game.create_sprite(2, 2)
+game.start_countdown(50000)
+game.set_score(0)
+
+def on_forever():
+    sprite.move(1)
+    basic.pause(100)
+    sprite.if_on_edge_bounce()
+    sprite.turn(Direction.RIGHT, 45)
+    sprite.if_on_edge_bounce()
+    if game.score() >= 5:
+        basic.pause(500)
+        basic.show_string("FASE 2")
+        basic.show_icon(IconNames.ANGRY)
+        basic.pause(500)
+        game.set_score(0)
+        sprite.move(2)
+        sprite.move(1)
+        basic.pause(80)
+        sprite.if_on_edge_bounce()
+        sprite.turn(Direction.RIGHT, 35)
+        sprite.if_on_edge_bounce()
+        game.set_score(0)
+        if game.score() == 3:
+            basic.pause(500)
+            basic.show_icon(IconNames.HAPPY)
+            music.play_melody("G B A G C5 B A B ", 120)
+            game.game_over()
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,11 +1,52 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
+    if (sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else {
+        game.removeLife(1)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        music.playMelody("C5 G B A F A C5 B ", 500)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("FASE SECRETA")
+})
+let sprite: game.LedSprite = null
+music.playMelody("C4:4 D4:4 E4:4 C4:4 D4:4 E4:4 C4:2", 120)
+basic.showString("OI")
+game.addLife(5)
+sprite = game.createSprite(2, 2)
+game.startCountdown(50000)
+game.setScore(0)
+basic.forever(function () {
     sprite.move(1)
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    basic.pause(100)
+    sprite.ifOnEdgeBounce()
     sprite.turn(Direction.Right, 45)
+    sprite.ifOnEdgeBounce()
+    if (game.score() >= 5) {
+        basic.pause(500)
+        basic.showString("FASE 2")
+        basic.showIcon(IconNames.Angry)
+        basic.pause(500)
+        game.setScore(0)
+        sprite.move(2)
+        sprite.move(1)
+        basic.pause(80)
+        sprite.ifOnEdgeBounce()
+        sprite.turn(Direction.Right, 35)
+        sprite.ifOnEdgeBounce()
+        game.setScore(0)
+        if (game.score() == 3) {
+            basic.pause(500)
+            basic.showIcon(IconNames.Happy)
+            music.playMelody("G B A G C5 B A B ", 120)
+            game.gameOver()
+        }
+    }
 })
-let sprite : game.LedSprite = null
-music.playMelody("E B C5 A B G A F ", 120)
-basic.showString("Oi, seja Bem Vindo!")
-basic.showString("Esse é somente um jogo teste")
-basic.showString("Espero que goste!")
